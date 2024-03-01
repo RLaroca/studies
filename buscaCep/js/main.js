777fab18e22203b299dba7067e72cafe2ab593c0
RLaroca <94089195+RLaroca@users.noreply.github.com>
2024-03-01 14:09:58 -0300

    adding functions to BuscaCep

--- buscaCep/js/main.js
+++ buscaCep/js/main.js
@@ -1,1 +1,27 @@
 
+function consulta(){
+    let cep = document.querySelector("#cep").value;
+
+    if(cep.length !== 8){
+        alert("Número de CEP inválido");
+        return
+    }
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            displayData(data);
+        })
+    })
+
+    function displayData(dados){
+        let display = document.getElementById("results");
+        display.innerHTML = `
+                                <p>${dados.logradouro}</p>
+                                <p>${dados.cep}</p>
+                                <p>${dados.bairro}</p>
+                                <p>${dados.localidade}</p>
+                            `
+    }
+
+}
